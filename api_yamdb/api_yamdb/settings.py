e73a5501a04446fa5edf9e63dafa6156ca1c9cff
Артём Ежов <dragster1001@yandex.ru>
2023-01-30 21:36:11 +0300

    api and reviews create

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -15,6 +15,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'api.apps.ApiConfig',
+    'reviews.apps.ReviewsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
