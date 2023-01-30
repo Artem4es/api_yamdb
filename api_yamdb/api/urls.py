f1fb81fd1b5739c4233f442967c672b12192c678
Dmitry Gerasimov <dmitryg@wiley.com>
2023-01-30 23:19:40 +0300

    feature/simple-jws: добавил библиотеку в requirements.txt Сделаны базовые наброски urls.py в приложении api , а также необходимые правки в settings.py

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,18 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+#router.register('users', UsersViewSet, basename='users')
+
+
+auth_patterns = [
+    path('signup/', SignUpView.as_view()),
+    path('token/', GetTokenView.as_view())
+]
+
+urlpatterns = [
+    path('v1/', include(router.urls)),
+    path('v1/auth/', include(auth_patterns)),
+]
+
