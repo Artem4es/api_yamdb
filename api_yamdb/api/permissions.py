1e259cfa52d0e0414d961523e412a72dd685994f
Dmitry-Ge <113944702+Dmitry-Ge@users.noreply.github.com>
2023-01-31 22:55:36 +0300

    feature/permissions: several fixes in permissions.py

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -13,6 +13,7 @@ class AdminOrReadOnly(BasePermission):
 
 
 class AuthorAdminModeratorPermission(BasePermission):
+
     def has_object_permission(self, request, view, obj):
         return request.method in SAFE_METHODS or (
             obj.author == request.user or request.user.is_superuser
