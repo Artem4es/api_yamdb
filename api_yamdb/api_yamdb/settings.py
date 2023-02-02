ad41dd5a603376cea70c8c2587e3c0f26ea7d8c0
Dmitry Gerasimov <q2digger@yandex.ru>
2023-02-02 22:16:43 +0300

    fixreview: added new application for users and auth.

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
     'reviews.apps.ReviewsConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
